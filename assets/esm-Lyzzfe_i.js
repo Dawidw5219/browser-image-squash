2cf95a06495fb9869a8ea457a7615ef5d5bd3c71
Dawid Wiewiórski <dawid.wiewiorski@app4you.dev>
2026-09-16 17:27:53 +0200

    docs: v1.1.0 (2026-09-16 17:27)

---
+++ assets/esm-Lyzzfe_i.js
@@ -0,0 +1,1 @@
+var e=()=>(async e=>{try{return typeof MessageChannel<`u`&&new MessageChannel().port1.postMessage(new SharedArrayBuffer(1)),WebAssembly.validate(e)}catch{return!1}})(new Uint8Array([0,97,115,109,1,0,0,0,1,4,1,96,0,0,3,2,1,0,5,4,1,3,1,1,10,11,1,9,0,65,0,254,16,2,0,26,11]));export{e as t};
